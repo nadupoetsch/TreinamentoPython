d555c8fc069fdda5a51bdc155716794c3fca1bc0
Bernardo Poetsch <nadupoetsch@gmail.com>
2023-12-26 14:25:23 -0300

    arrumando os nomes dos arquivos

--- TipoVariavel.py
+++ TipoVariavel.py
@@ -1,11 +1,11 @@
 codigo = 10
 salario = 1500.00
 nome = 'jose'
-situação = True
+situacao = True
 
 tipo = type (salario)
 
 # print(salario)
 # print(tipo)
 
-print("codigo: ", codigo, "Nome: ", nome, "O salário atual é de ", salario)
+print("codigo:",codigo,"Nome:",nome,"O salário atual é de",salario)
