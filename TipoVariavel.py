bca91218c12ae2e9c6eab1bb81df35a93721d434
Bernardo Poetsch <nadupoetsch@gmail.com>
2023-12-26 13:10:42 -0300

    primeiro upload

---
+++ TipoVariavel.py
@@ -0,0 +1,11 @@
+codigo = 10
+salario = 1500.00
+nome = 'jose'
+situação = True
+
+tipo = type (salario)
+
+# print(salario)
+# print(tipo)
+
+print("codigo: ", codigo, "Nome: ", nome, "O salário atual é de ", salario)
